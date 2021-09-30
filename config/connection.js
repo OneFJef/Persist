47fb1af2ba0da4a1024bed1541b965109b9d40a2
Jef Mitchell <jef@project87.net>
2021-09-29 21:33:27 -0400

    Update SQL - (connection.js) SQL connection peramiters changed.

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -12,8 +12,8 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_PASSWORD,
     {
       host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+      dialect: 'postgres',
+      port: 5432
     }
   );
 }

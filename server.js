c9e6b5a2cce76c9028f53ac1be90d38e24941a90
Tyler Smith <mrtbird539@gmail.com>
2021-10-02 10:07:35 -0400

    added Auth0

--- server.js
+++ server.js
@@ -14,15 +14,24 @@ const PORT = process.env.PORT || 3001;
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
-const sess = {
-  secret: process.env.SESSION_SECRET,
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'https://ancient-ravine-93786.herokuapp.com',
+  clientID: 'NOlcPn22ZIK5NqyWdor03o61uJfSh72X',
+  issuerBaseURL: 'https://dev-cx6ypp69.us.auth0.com'
 };
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 app.use(session(sess));
 

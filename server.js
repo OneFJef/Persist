db7bb32c6f47f2df1f63bd19026847ef6bbe87cf
Alex Parker <alex.n.parker82@gmail.com>
2021-10-02 12:52:36 -0400

    getting routes to work

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
 
 
 const config = {
   authRequired: false,
   auth0Logout: true,
   secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'https://ancient-ravine-93786.herokuapp.com',
+  baseURL: 'http://localhost:3001' || 'https://ancient-ravine-93786.herokuapp.com',
   clientID: 'NOlcPn22ZIK5NqyWdor03o61uJfSh72X',
   issuerBaseURL: 'https://dev-cx6ypp69.us.auth0.com'
 };
@@ -28,9 +28,9 @@ const config = {
 app.use(auth(config));
 
 // req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
+// app.get('/', (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// });
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
@@ -43,5 +43,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port:${PORT}`));
 });

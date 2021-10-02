e528eca5b4a2c8e559c6663f0ddc4921ef2564ed
Jef Mitchell <jef@project87.net>
2021-10-02 13:43:59 -0400

    Update SQL - (seed.sql) Fixed the id.

--- db/seed.sql
+++ db/seed.sql
@@ -1,33 +1,33 @@
 INSERT INTO
-    `user` (id, nickname, email)
+    `user` (nickname, email)
 VALUES
-    (1, 'alex123', 'persist-alex@mailinator.com'),
-    (2, 'kelley123', 'persist-kelley@mailinator.com'),
-    (3, 'jef123', 'persist-jef@mailinator.com'),
-    (4, 'josef123', 'persist-josef@mailinator.com'),
-    (5, 'tyler123', 'persist-tyler@mailinator.com');
+    ('alex123', 'persist-alex@mailinator.com'),
+    ('kelley123', 'persist-kelley@mailinator.com'),
+    ('jef123', 'persist-jef@mailinator.com'),
+    ('josef123', 'persist-josef@mailinator.com'),
+    ('tyler123', 'persist-tyler@mailinator.com');
 
 INSERT INTO
-    `task` (id, color, category, category_sub, `day`, `hours`, `user_id`)
+    `task` (color, category, category_sub, `day`, `hours`, `user_id`)
 VALUES
-    (1, 'black', 'fitness', '', 0, 4, 1),
-    (2, 'black', 'fitness', '', 1, 4, 1),
-    (3, 'black', 'fitness', '', 2, 4, 1),
-    (4, 'black', 'fitness', '', 3, 4, 1),
-    (5, 'black', 'fitness', '', 4, 4, 1),
-    (6, 'black', 'fitness', '', 5, 4, 1),
-    (7, 'black', 'fitness', '', 6, 4, 1),
-    (8, 'white', 'sleep', '', 0, 4, 1),
-    (9, 'white', 'sleep', '', 1, 4, 1),
-    (10, 'white', 'sleep', '', 2, 4, 1),
-    (11, 'white', 'sleep', '', 3, 4, 1),
-    (12, 'white', 'sleep', '', 4, 4, 1),
-    (13, 'white', 'sleep', '', 5, 4, 1),
-    (14, 'white', 'sleep', '', 6, 4, 1),
-    (15, 'red', 'work', '', 0, 8, 1),
-    (16, 'red', 'work', '', 1, 8, 1),
-    (17, 'red', 'work', '', 2, 8, 1),
-    (18, 'red', 'work', '', 3, 8, 1),
-    (19, 'red', 'work', '', 4, 8, 1),
-    (20, 'red', 'work', '', 5, 8, 1),
-    (21, 'red', 'work', '', 6, 8, 1);
+    ('black', 'fitness', '', 0, 4, 1),
+    ('black', 'fitness', '', 1, 4, 1),
+    ('black', 'fitness', '', 2, 4, 1),
+    ('black', 'fitness', '', 3, 4, 1),
+    ('black', 'fitness', '', 4, 4, 1),
+    ('black', 'fitness', '', 5, 4, 1),
+    ('black', 'fitness', '', 6, 4, 1),
+    ('white', 'sleep', '', 0, 4, 1),
+    ('white', 'sleep', '', 1, 4, 1),
+    ('white', 'sleep', '', 2, 4, 1),
+    ('white', 'sleep', '', 3, 4, 1),
+    ('white', 'sleep', '', 4, 4, 1),
+    ('white', 'sleep', '', 5, 4, 1),
+    ('white', 'sleep', '', 6, 4, 1),
+    ('red', 'work', '', 0, 8, 1),
+    ('red', 'work', '', 1, 8, 1),
+    ('red', 'work', '', 2, 8, 1),
+    ('red', 'work', '', 3, 8, 1),
+    ('red', 'work', '', 4, 8, 1),
+    ('red', 'work', '', 5, 8, 1),
+    ('red', 'work', '', 6, 8, 1);

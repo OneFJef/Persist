937ce2f15cd313bdf6ea37e867bf7f4956405efe
Jef Mitchell <jef@project87.net>
2021-10-02 11:13:16 -0400

    Update Files - (projectData.json) Deleted - (seed.js) Deleted - (userData.json) Deleted
    SQL
    - (schema.sql) Updated with project schema.

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,19 @@
-DROP DATABASE IF EXISTS crowdfund_db;
-CREATE DATABASE crowdfund_db;
+CREATE TABLE `user_table` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `auth_token` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `task_table` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `color` varchar(50) DEFAULT NULL,
+  `category` varchar(50) NOT NULL,
+  `category-sub` varchar(50) DEFAULT NULL,
+  `day` int NOT NULL,
+  `hours` int NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `task_table_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_table` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;

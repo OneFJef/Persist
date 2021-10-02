82176d672206ee1063b681228804291913cc4b0a
Tyler Smith <mrtbird539@gmail.com>
2021-10-02 10:36:37 -0400

    Updated Auth0

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { auth } = require('express-openid-connect');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 3001;
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
-const { auth } = require('express-openid-connect');
 
 const config = {
   authRequired: false,
@@ -32,8 +31,6 @@ app.use(auth(config));
 app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
-
-app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);

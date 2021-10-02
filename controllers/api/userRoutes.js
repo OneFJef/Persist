c36f5d393c658288b4fd483bc6d802eaff812f94
Jas <ragnarock.josef@gmail.com>
2021-10-02 12:45:54 -0400

    user routes fix

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -10,18 +10,22 @@ router.post('/', async (req, res) => {
       email: req.body.email,
       password: req.body.password,
     });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
-    // Logout
-    router.post('/logout', (req, res) => {
-      // When the user logs out, destroy the session
-      if (req.session.loggedIn) {
-        req.session.destroy(() => {
-          res.status(204).end();
-        });
-      } else {
-        res.status(404).end();
-      }
+// Logout
+router.post('/logout', (req, res) => {
+  // When the user logs out, destroy the session
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
     });
+  } else {
+    res.status(404).end();
+  }
+});
 
-    module.exports = router;
+module.exports = router;

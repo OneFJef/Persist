61c2dd5df9a62898f06dda4ebe0f0e2093f9a67f
Jef Mitchell <jef@project87.net>
2021-10-04 21:54:13 -0400

    Update Javascript - (homeRoutes.js) Auto format - (controllers/api/index.js) Auto format - (taskRoutes.js) removed the day column. - (index.js) Changed hasMany to belongsToMany.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,66 +1,62 @@
-const router = require('express').Router();
-const { User, Task } = require('../models');
+const router = require("express").Router();
+const { User, Task, Day } = require("../models");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const taskData = await Task.findAll();
     const tasks = taskData.map((task) => task.get({ plain: true }));
 
-    res.render('homepage', { 
+    res.render("homepage", {
       tasks,
-      logged_in: req.oidc.isAuthenticated()});
+      logged_in: req.oidc.isAuthenticated(),
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-
-router.get('/day', async (req, res) => {
+router.get("/day", async (req, res) => {
   try {
-    const taskData = await Task.findAll();
+    const taskData = await Task.findAll({
+      include: [{ model: Day }],
+    });
     const tasks = taskData.map((task) => task.get({ plain: true }));
-    res.render('day', { tasks });
-
+    res.render("day", { tasks });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/newtask', async (req, res) => {
+router.get("/newtask", async (req, res) => {
   try {
-
-    res.render('newtask');
+    res.render("newtask");
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-
-router.get('/week', async (req, res) => {
+router.get("/week", async (req, res) => {
   try {
-
-    res.render('week');
+    res.render("week");
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 
 // Go to a specific task
-router.get('/task/:id', async (req, res) => {
+router.get("/task/:id", async (req, res) => {
   try {
     const taskData = await Task.findByPk(req.params.id);
     const task = taskData.get({ plain: true });
 
-    res.render('task', { task });
+    res.render("task", { task });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-
 // Edit a specific task
-router.put('/task/:id', async (req, res) => {
+router.put("/task/:id", async (req, res) => {
   try {
     const taskData = await Task.update(req.body, {
       where: {
@@ -73,21 +69,20 @@ router.put('/task/:id', async (req, res) => {
     }
 
     const task = taskData.get({ plain: true });
-    res.render('task', { task });
-
+    res.render("task", { task });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect("/profile");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;

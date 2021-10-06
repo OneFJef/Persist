6a1aaf885b9c2b480c2620317d743d411d2cb48b
Jef Mitchell <jef@project87.net>
2021-10-06 19:09:26 -0400

    Updated schema.sql column names.

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -17,3 +17,13 @@ CREATE TABLE `task` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `task_table_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `day` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `day` int NOT NULL,
+  `task_id` int NOT NULL,
+  `activity_length` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `task_id` (`task_id`),
+  CONSTRAINT `day_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;

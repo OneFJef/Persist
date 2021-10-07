146b122e1229e9ef67e36bbf1392b50b4f14867d
Jef Mitchell <jef@project87.net>
2021-10-06 21:14:40 -0400

    removed username model.

--- models/User.js
+++ models/User.js
@@ -12,10 +12,6 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,

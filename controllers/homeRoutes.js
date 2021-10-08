dd4415ecc15689362682ab7fdc2ae65106f63daa
Jas <ragnarock.josef@gmail.com>
2021-10-08 18:39:36 -0400

    added day functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const dayjs = require("dayjs");
 const { User, Task, Day } = require("../models");
 
 router.get("/", async (req, res) => {
@@ -17,16 +18,32 @@ router.get("/", async (req, res) => {
 
 router.get("/day", async (req, res) => {
   try {
-    const taskData = await Task.findAll({
-      include: [{ model: Day }],
-    });
+    const taskData = await Task.findAll({});
+    const dayData = await Day.findAll();
+
+    const day = dayData.map((day) => day.get({ plain: true }));
     const tasks = taskData.map((task) => task.get({ plain: true }));
-    res.render('day', { 
+    res.render('day', {
+      day,
       tasks,
       logged_in: req.oidc.isAuthenticated()
-     });
+    });
 
   } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/buildDay", async (req, res) => {
+  try {
+    const dayData = await Day.findAll();
+
+    const days = dayData.map((day) => day.get({ plain: true }));
+    res.json(days);
+
+  } catch (err) {
+    console.error(err);
     res.status(500).json(err);
   }
 });
@@ -34,7 +51,7 @@ router.get("/day", async (req, res) => {
 router.get("/newtask", async (req, res) => {
   try {
 
-    res.render('newtask', {logged_in: req.oidc.isAuthenticated()})
+    res.render('newtask', { logged_in: req.oidc.isAuthenticated() })
   } catch (err) {
     res.status(500).json(err);
   }
@@ -43,7 +60,7 @@ router.get("/newtask", async (req, res) => {
 router.get("/week", async (req, res) => {
   try {
 
-    res.render('week', {logged_in: req.oidc.isAuthenticated()});
+    res.render('week', { logged_in: req.oidc.isAuthenticated() });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -55,7 +72,7 @@ router.get("/task/:id", async (req, res) => {
     const taskData = await Task.findByPk(req.params.id);
     const task = taskData.get({ plain: true });
 
-    res.render('task', { 
+    res.render('task', {
       task,
       logged_in: req.oidc.isAuthenticated()
     });
@@ -73,6 +90,68 @@ router.get('/login', (req, res) => {
   }
 
   res.render("login");
+});
+
+// Get task data
+router.get("/tasks", async (req, res) => {
+  try {
+    const taskData = await Task.findAll();
+    const tasks = taskData.map((task) => task.get({ plain: true }));
+
+    res.json(tasks);
+  } catch (err) {
+    console.error(err);
+  };
+});
+
+// Generate task json data 
+router.get("/taskData/:id", async (req, res) => {
+  try {
+    const taskData = await Task.findByPk(req.params.id);
+    const task = taskData.get({ plain: true });
+
+    res.json(task);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// Add task to day
+router.post("/day", async (req, res) => {
+  try {
+
+    const { day, task_id, activity_length } = req.body;
+    const dayData = await Day.create({ day, task_id, activity_length });
+
+    res.status(200).json(dayData);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  };
+});
+
+// Remove task from day
+router.delete("/day/:id", async (req, res) => {
+  try {
+    const dayData = await Day.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!dayData) {
+      res.status(404).json({ message: "No day entry found with that id!" });
+      return;
+    };
+
+    res.status(200).json(dayData);
+
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
---
+++ public/js/day.js
@@ -0,0 +1,230 @@
+let hoursDiv = $(".hours-div");
+const taskSubmit = $(".task-submit");
+const taskName = $(".task-name");
+let todayDiv = $(".today");
+let totalTaskHrs = $(".countHours");
+
+let taskHours = [];
+
+let currentHourTotal = 0;
+const dayHours = 24;
+
+// Returns the day as a string value
+const todayDay = (day) => {
+    switch (day) {
+        case 0:
+            return "Sunday";
+
+        case 1:
+            return "Monday";
+
+        case 2:
+            return "Tuesday";
+
+        case 3:
+            return "Wednesday";
+
+        case 4:
+            return "Thursday";
+
+        case 5:
+            return "Friday";
+
+        case 6:
+            return "Saturday";
+    };
+};
+
+const onPageLoad = () => {
+    let storedHours = JSON.parse(localStorage.getItem("todayTaskHours"));
+    if (storedHours !== null) {
+        taskHours = storedHours;
+        localStorage.setItem("todayTaskHours", JSON.stringify(taskHours));
+    };
+
+};
+
+const deleteTask = (id) => {
+
+    $.ajax({
+        url: "buildDay",
+        method: "GET"
+    }).then((response) => {
+        let currentDay = dayjs().day();
+        let taskIds = [];
+        let idToDelete = [];
+
+        for (let i = 0; i < response.length; i++) {
+            if (response[i].day == currentDay) {
+                taskIds.push(response[i]);
+            };
+        };
+
+        for (let i = 0; i < taskIds.length; i++) {
+            if (taskIds[i].task_id = id) {
+                idToDelete.push(taskIds[i].id);
+            };
+        };
+        $.ajax({
+            url: `/day/${idToDelete[0]}`,
+            method: "DELETE",
+        }).then((response) => {
+            console.log("Success", response);
+        });
+    });
+};
+
+// Creates the task HTML element and appends it
+const createAndAppendTask = (header, paragraph, hours, id) => {
+
+    //Render the task
+    let columnAdd = $("<div>").addClass("column");
+    let sectionDiv = $("<div>").addClass("content blue");
+    let sectionHead = $("<h1>").html(header);
+    let description = $("<p>").html(paragraph);
+    let hoursPara = $("<p>").addClass("countHours").html(hours + "hrs");
+    let shownButt = $("<div>").addClass("show");
+    let removeButt = $("<button>").addClass("button is-danger").html("Remove");
+
+    shownButt.append(removeButt);
+    sectionDiv.append(sectionHead, description, hoursPara, shownButt);
+    columnAdd.append(sectionDiv);
+    hoursDiv.append(columnAdd);
+
+    removeButt.on("click", (e) => {
+        e.preventDefault();
+        columnAdd.addClass("hide");
+        deleteTask(id);
+    });
+};
+
+// Creates the the controls of the hidden HTML elements
+const createHiddenControls = (response) => {
+
+    //Hidden until conditions are met
+    let hiddenControls = $("<div>").addClass("hide");
+    let hiddenCaption = $("<h3>").html("Did you complete this on time?");
+    let hiddenField = $("<div>").addClass("field is-grouped");
+    let hiddenSuccess = $("<div>").addClass("control success");
+    let hiddenFailure = $("<div>").addClass("control failure");
+    let hiddenSuccButt = $("<button>").addClass("button is-link is-success").attr("type", "button").html("Yes");
+    let hiddenFailButt = $("<button>").addClass("button is-link is-danger").attr("type", "button").html("No");
+    let hiddenLabel = $("label").addClass("label").html("How many hours did it take?");
+    let hiddenTimeControl = $("<div>").addClass("control");
+    let hiddenTimeInput = $("<input>").addClass("input").attr("number");
+};
+
+// Loads all of the daily tasks from the DB
+const loadDay = () => {
+    $.ajax({
+        url: "/buildDay",
+        method: "GET",
+    }).then((response) => {
+        let currentDay = dayjs().day();
+        let taskIds = [];
+
+        for (let i = 0; i < response.length; i++) {
+            if (response[i].day == currentDay) {
+                taskIds.push(response[i].task_id);
+            };
+        };
+        for (let i = 0; i < taskIds.length; i++) {
+            $.ajax({
+                url: `/taskData/${taskIds[i]}`,
+                method: "GET",
+            }).then((response) => {
+                createAndAppendTask(response.category, response.category_sub, response.hours, response.id);
+            });
+        };
+    });
+};
+
+// Add the selected task to the current day in the DB
+const addDayToDB = (id, length) => {
+    const dayData = {
+        day: dayjs().day(),
+        task_id: id,
+        activity_length: length
+    };
+
+    $.ajax({
+        url: `/day`,
+        method: "POST",
+        data: dayData,
+    }).then((response) => {
+        console.log("Success", response);
+    });
+};
+
+//Checks to see if the day has it's hours full
+const checkHours = (newHours) => {
+    const dayHours = 24;
+    const newTaskHours = [];
+    $.ajax({
+        url: "/buildDay",
+        method: "GET",
+    }).then((response) => {
+        let currentDay = dayjs().day();
+
+        for (let i = 0; i < response.length; i++) {
+            if (response[i].day == currentDay) {
+                newTaskHours.push(response[i].activity_length);
+                localStorage.setItem("todayTaskHours", JSON.stringify(newTaskHours));
+            };
+        };
+    });
+
+    let savedHours = JSON.parse(localStorage.getItem("todayTaskHours"));
+    if (!savedHours) {
+        return;
+    }
+    const existingHours = savedHours.reduce(function (a, b) {
+        return a + b;
+    });
+
+    const totalHours = newHours + existingHours;
+    if (totalHours <= dayHours) {
+        return true;
+    };
+    return false;
+}
+
+// Adds the selected task to the day if there is enough time remaining in the day
+const addTaskToDay = () => {
+    taskSubmit.on("click", () => {
+        if (taskName.val() !== "Add an activity") {
+            $.ajax({
+                url: "/tasks",
+                method: "GET",
+            }).then((response) => {
+                const categoryName = taskName.val();
+                for (let i = 0; i < response.length; i++) {
+                    if (response[i].category == categoryName) {
+                        const timeLeft = checkHours(response[i].hours);
+                        if (timeLeft === true) {
+                            createAndAppendTask(response[i].category, response[i].category_sub, response[i].hours, response[i].id);
+                            addDayToDB(response[i].id, response[i].hours);
+                        } else {
+                            console.log("Too many Hours");
+                        }
+                    };
+                };
+            });
+        } else {
+            console.log("error");
+        };
+    });
+};
+
+// Starts the functionality on page load
+const init = () => {
+    onPageLoad();
+    loadDay();
+    let dayCaption = $("<h1>").html(todayDay(dayjs().day()));
+    todayDiv.append(dayCaption);
+    taskSubmit.on("click", addTaskToDay());
+    checkHours(0);
+};
+
+init();
+
